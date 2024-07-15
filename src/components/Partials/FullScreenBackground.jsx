5e201065eda189e75fbbf3ba43d798ad90d7f957
Shariar Hasan Emon <imemon.sh@gmail.com>
2024-07-15 07:29:39 +0600

    backgroud opacity updated

--- src/components/Partials/FullScreenBackground.jsx
+++ src/components/Partials/FullScreenBackground.jsx
@@ -5,7 +5,7 @@ export default function FullScreenBackground({ children }) {
     return (
         <div className="bg-cover bg-center h-screen w-full flex items-center justify-center relative"
              style={{ backgroundImage: `url(${bgImage})` }}>
-            <div className="absolute top-0 left-0 h-full w-full bg-black opacity-30"></div>
+            <div className="absolute top-0 left-0 h-full w-full bg-black opacity-60"></div>
             <div className="relative z-10 container mx-auto px-4">
                 {children}
             </div>
